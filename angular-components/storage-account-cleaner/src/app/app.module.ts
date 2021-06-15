096e2d852853ff41d63281c3c64d7403c294bf1a
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-06-15 01:16:36 -0400

    sanity commit

--- angular-components/storage-account-cleaner/src/app/app.module.ts
+++ angular-components/storage-account-cleaner/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RetentionPolicyEditorModule } from './retention-policy-editor/retention-policy-editor.module';
 import { NotcoreModule } from './notcore/notcore.module';
-
+import { ApiModule } from 'appliance-apiclient';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, RetentionPolicyEditorModule, NotcoreModule
+    BrowserModule, RetentionPolicyEditorModule,
+    NotcoreModule,
+    ApiModule
   ],
   exports: [RetentionPolicyEditorModule],
   providers: [],
