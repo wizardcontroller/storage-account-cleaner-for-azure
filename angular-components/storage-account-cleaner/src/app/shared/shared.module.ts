fb77d49e4ad4856c75431b5b9c57101270e3f1db
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-06-16 22:37:16 -0400

    api update sanity commit

---
+++ angular-components/storage-account-cleaner/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { ApiModule } from 'appliance-apiclient';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedComponent } from './shared.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SharedComponent]
+})
+export class SharedModule { }
