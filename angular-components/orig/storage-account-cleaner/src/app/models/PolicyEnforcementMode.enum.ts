2ea61d2a5b671ef6a0f77cfb5af231168f40123a
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-06-18 15:27:19 -0400

    angular app structure refactor

--- angular-components/orig/storage-account-cleaner/src/app/models/PolicyEnforcementMode.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum PolicyEnforcementMode {
-  WhatIf, ApplyPolicy
-}
--- angular-components/orig/storage-account-cleaner/src/app/notcore/notcore.module.ts
+++
@@ -1,26 +0,0 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-// as per Pluralsight Angular Architecture and Best Practicves
-
-import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NotcoreComponent } from './notcore.component';
-import { EnsureModuleLoadedOnceGuard } from './EnsureModuleLoadedOnceGuard';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-
-@NgModule({
-  imports: [
-    CommonModule, HttpClientModule
-  ],
-  declarations: [NotcoreComponent],
-  providers:[
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true  }
-
-  ]
-})
-
-export class NotcoreModule extends EnsureModuleLoadedOnceGuard {
-  constructor(@Optional() @SkipSelf() parentModule: NotcoreModule){
-    super(parentModule);
-  }
- }
