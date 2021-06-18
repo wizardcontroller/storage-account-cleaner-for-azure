2d42ea49700c088dab1371b76e094db10c96d9db
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-06-18 17:50:24 -0400

    fixing base path environment variable name

--- angular-components/dashboard/src/environments/environment.prod.ts
+++ angular-components/dashboard/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_BASE_PATH: '/'
+  BASE_PATH: '/'
 };
--- angular-components/dashboard/src/environments/environment.ts
+++ angular-components/dashboard/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_BASE_PATH: '/'
+  BASE_PATH: '/'
 };
 
 /*
