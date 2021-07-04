200cbde99b83167b628ba7eac4b9c33f421fb431
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-07-04 10:51:59 -0400

    code complete on routing integration

--- angular-components/dashboard/src/app/app-routing.module.ts
+++ angular-components/dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ApplianceContextViewComponent } from './shared/display-templates/ApplianceContextView/ApplianceContextView.component';
+import { ApplianceContextService } from './shared/display-templates/services/ApplianceContext.service';
+import { StorageAccountViewComponent } from './shared/display-templates/StorageAccountView/StorageAccountView.component';
+import { SubscriptionsViewComponent } from './shared/display-templates/SubscriptionsView/SubscriptionsView.component';
 
-const routes: Routes = [{ path: '**', component: AppComponent }];
+const routes: Routes = [
+
+
+
+  { path: 'workbench', component: ApplianceContextViewComponent },
+  { path: '', redirectTo: 'workbench', pathMatch: 'full'},
+  { path: '**', component: ApplianceContextViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
