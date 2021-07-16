f27dbf31e333bc50b4d5e8dc96a7d11665fbec1e
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-07-16 01:38:25 -0400

    auth interceptor logic update

--- angular-components/dashboard/src/app/interceptors/auth-header.interceptor.ts
+++ angular-components/dashboard/src/app/interceptors/auth-header.interceptor.ts
@@ -3,23 +3,24 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiConfigService } from '../core/ApiConfig.service';
 import { OperatorPageModel } from '@wizardcontroller/sac-appliance-lib/';
 import { ApplianceApiService } from '../shared/services/appliance-api.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import {CoreModule} from '../core/core.module'
+import { CoreModule } from '../core/core.module';
 @Injectable()
 export class AuthHeaderInterceptor implements HttpInterceptor {
   private pageModel!: OperatorPageModel;
   private applianceUrl!: string;
-  private HEADER_IMPERSONATION_TOKEN = "x-table-retention-mgmt-impersonation";
-  private HEADER_X_ZUMO_AUTH = "X-ZUMO-AUTH";
-  private HEADER_CURRENTSUBSCRIPTION = "x-table-retention-current-subscription";
-  private HEADER_CURRENT_STORAGE_ACCOUNT = "x-table-retention-current-storage-account";
-  private interceptorIsReady : boolean = false;
+  private HEADER_IMPERSONATION_TOKEN = 'x-table-retention-mgmt-impersonation';
+  private HEADER_X_ZUMO_AUTH = 'X-ZUMO-AUTH';
+  private HEADER_CURRENTSUBSCRIPTION = 'x-table-retention-current-subscription';
+  private HEADER_CURRENT_STORAGE_ACCOUNT =
+    'x-table-retention-current-storage-account';
+  private interceptorIsReady: boolean = false;
 
   constructor(private apiConfigSvc: ApiConfigService) {
     this.apiConfigSvc.operatorPageModelChanges$.subscribe((data) => {
@@ -27,8 +28,6 @@ export class AuthHeaderInterceptor implements HttpInterceptor {
       this.applianceUrl = this.pageModel?.applianceUrl?.toString() as string;
       this.interceptorIsReady = true;
     });
-
-
   }
 
   intercept(
@@ -40,24 +39,25 @@ export class AuthHeaderInterceptor implements HttpInterceptor {
     /**
      * this interceptor depends on the current pagemodel context
      */
-    if(this.interceptorIsReady)
-    {
+    if (this.interceptorIsReady) {
+      if (requestClone.url.includes(this.applianceUrl)) {
+        console.log('http interceptor is adding xumo auth header');
+        if (!requestClone.headers.has(this.HEADER_X_ZUMO_AUTH)) {
+          requestClone.headers.append(
+            this.HEADER_X_ZUMO_AUTH,
+            this.pageModel?.easyAuthAccessToken?.toString() as string
+          );
+        }
 
-    if (requestClone.url.includes(this.applianceUrl)) {
-
-      console.log("http interceptor is adding xumo auth header");
-      requestClone.headers.append(
-        this.HEADER_X_ZUMO_AUTH,
-        this.pageModel?.easyAuthAccessToken?.toString() as string
-      );
-
-      console.log("http interceptor is adding impersonation token header");
-      requestClone.headers.append(
-        this.HEADER_IMPERSONATION_TOKEN,
-        this.pageModel?.impersonationToken?.toString() as string
-      );
-
-    }}
+        console.log('http interceptor is adding impersonation token header');
+        if (!requestClone.headers.has(this.HEADER_IMPERSONATION_TOKEN)) {
+          requestClone.headers.append(
+            this.HEADER_IMPERSONATION_TOKEN,
+            this.pageModel?.impersonationToken?.toString() as string
+          );
+        }
+      }
+    }
     return next.handle(requestClone);
   }
 }
