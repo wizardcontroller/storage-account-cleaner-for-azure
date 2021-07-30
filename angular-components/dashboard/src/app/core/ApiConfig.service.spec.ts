30156871c3e9ef2825c706a998775f023c347f0c
ataxlab <85252545+ataxlab@users.noreply.github.com>
2021-07-30 13:21:22 -0400

    test cleanup sanity commit

--- angular-components/dashboard/src/app/core/ApiConfig.service.spec.ts
+++ angular-components/dashboard/src/app/core/ApiConfig.service.spec.ts
@@ -3,13 +3,14 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MessageService } from 'primeng/api';
 import { ApiConfigService } from './ApiConfig.service';
 
 describe('Service: ApiConfig', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule, RouterTestingModule],
-      providers: [ApiConfigService]
+    imports: [ HttpClientTestingModule, RouterTestingModule````],
+      providers: [ApiConfigService, MessageService]
     });
   });
 
